af900e33bcaa22cad8c9e3b18bebf4355f12c96f
Ian McKenzie <imckenzie777@gmail.com>
2020-05-01 21:46:50 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,16 +1,14 @@
 # Examples inspired by U. S. National Organic Standards
 
-#ORGANIC_PRODUCE =
-[
+ORGANIC_PRODUCE = [
   "Strawberries",
   "Potatoes",
   "Grapes",
   "Avocadoes",
   "Asparagus"
-],
+]
 
-#CONVENTIONAL_PRODUCE =
-[
+CONVENTIONAL_PRODUCE = [
   "Grapefruit",
   "Pineapple",
   "Oranges",
